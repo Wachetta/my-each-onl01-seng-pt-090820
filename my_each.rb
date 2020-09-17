b557c40ea45204dcd594e32f0d59e965e3eb4972
Zachary Darneille <zakdarn@gmail.com>
2020-09-17 23:01:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,6 @@
 require 'pry'
 def my_each(words) #put argument(s) here
+<<<<<<< HEAD
 if block_given?
   counter = 0
     while counter < words.length
@@ -10,4 +11,14 @@ if block_given?
       else
         words
       end
+=======
+if array = []
+  puts "This block should not run."
+  counter = 0
+    while counter < words.length
+    yield(words)
+#binding.pry
+    counter +=  1
+  end
+>>>>>>> a4e9acc2eb6beb63165a9bc28cd3058a77b5e0bb
 end
